1dc6fa77c84c2af8a474a6c217a4136940004651
dhvanil-7 <110315539+dhvanil-7@users.noreply.github.com>
2024-09-14 19:17:41 -0400

    feat: ADD nutrition info

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,16 +19,17 @@ my_dataframe = session.table("smoothies.public.fruit_options").select("fruit_nam
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients: ",
     my_dataframe,
-    max_selections=5
-    
+    max_selections=5   
 )
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-
 if ingredients_list:
+    ingredients_string = ""
 
-    ingredients_string= ", ".join(ingredients_list)
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + " "
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     my_insert_stmt = f""" insert into smoothies.public.orders(ingredients, name_on_order)
     values ('{ingredients_string}', '{customer_name}')
